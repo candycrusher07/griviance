8015679c07f4d1f117aa7ddf740580263aa7f094
candycrusher07 <123592519+candycrusher07@users.noreply.github.com>
2025-05-15 20:16:17 +0530

    Update jk.py

--- jk.py
+++ jk.py
@@ -63,7 +63,7 @@ st.markdown("""
 
     /* Text input */
     .stTextInput > div > div > input {
-        background-color: #800040 !important;
+        background-color: #ffe6f0!important;
         color: #800040 !important;
         font-family: 'Georgia', serif !important;
         border: 2px solid #ff99cc;
